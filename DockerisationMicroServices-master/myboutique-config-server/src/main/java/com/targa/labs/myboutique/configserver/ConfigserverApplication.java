01c6279d67636acbbed11d00c5441b83f038927f
emna <bhsemna90@gmail.com>
2021-05-25 21:19:44 +0100

    push to jenkins

--- DockerisationMicroServices-master/myboutique-config-server/src/main/java/com/targa/labs/myboutique/configserver/ConfigserverApplication.java
+++ DockerisationMicroServices-master/myboutique-config-server/src/main/java/com/targa/labs/myboutique/configserver/ConfigserverApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigserverApplication {
-//test push for jenkins
+//testt push for jenkins
     public static void main(String[] args) {
         SpringApplication.run(ConfigserverApplication.class, args);
     }
